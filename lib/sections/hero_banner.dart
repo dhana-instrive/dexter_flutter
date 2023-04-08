3435da252323cf68ff4468e1d6099d73052452e8
dhanasra <dhana.spiderlingz@gmail.com>
2023-04-08 14:45:47 +0530

    feat : push code

--- lib/sections/hero_banner.dart
+++ lib/sections/hero_banner.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/constants.dart';
+
 class HeroBanner extends StatefulWidget {
   const HeroBanner({super.key});
 
@@ -11,7 +13,7 @@ class _HeroBannerState extends State<HeroBanner> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("Victory", style: TextStyle(color: Colors.black),),),
+      child: Center(child: Text(welcomeText, style: TextStyle(color: Colors.black),),),
     );
   }
 }
