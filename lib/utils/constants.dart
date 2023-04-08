096b9a1d5dd7c910e7803227e6b581f56ca09210
dhana-instrive <120463647+dhana-instrive@users.noreply.github.com>
2023-04-08 20:41:24 +0530

    Updated file

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
-const welcomeText = 'Welcome Friend123';
 
-const genPrimaryColor = "#A4BC92";
-const genSecondaryColor = "#B3C99C";
-const genTertiaryColor = "#C7E9B0";
+        const genPrimaryColor = "#FF0000";
+        const genSecondaryColor = "#B3C99C";
+        const genTertiaryColor = "#C7E9B0";
+        
