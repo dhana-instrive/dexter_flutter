7ebbf3a566a93c57b5fe39bcc2b5f58128452412
dhana-instrive <120463647+dhana-instrive@users.noreply.github.com>
2023-04-12 11:34:41 +0530

    Updated file

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,22 +1,23 @@
-const genPrimaryColor = "";
-const genSecondaryColor = "";
-const genTertiaryColor = "";
 
-const navBarName = "";
-const navBarActions = [];
-const navBarLogo = "";
+        const genPrimaryColor = "#FF0000";
+        const genSecondaryColor = "#002200";
+        const genTertiaryColor = "#123233";
+        const navBarName = "Textile App";
+        const navBarActions = ['Home','Contact','Feedback'];
+        const navBarLogo = "";
 
-const bannerTitle="";
-const bannerDesc="";
-const bannerBgImage="";
-const bannerButtontxt="";
+        const bannerTitle="My Textile";
+        const bannerDesc="This is a model desc text for textile shop...";
+        const bannerBgImage="https://images.unsplash.com/photo-1618901185975-d59f7091bcfe?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw0Mjk5MjR8MHwxfHNlYXJjaHwxfHxzYXJlZXxlbnwwfHx8fDE2ODA5NjYxMjM&ixlib=rb-4.0.3&q=85";
+        const bannerButtontxt="Click";
 
-const aboutImage="";
-const aboutTitle="";
-const aboutTxt="";
+        const aboutImage="https://images.unsplash.com/photo-1618901185975-d59f7091bcfe?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw0Mjk5MjR8MHwxfHNlYXJjaHwxfHxzYXJlZXxlbnwwfHx8fDE2ODA5NjYxMjM&ixlib=rb-4.0.3&q=85";
+        const aboutTitle="About Us";
+        const aboutTxt="This is a model about text...";
 
-const testimonial=[];
+        const testimonial=[{'content':'This is a good shop','userName':'dhana'}];
 
-const footerCopyright="";
-const footerLink1="";
-const footerLink2="";
+        const footerCopyright="Instrive";
+        const footerLink1="Privacy Policy";
+        const footerLink2="Terms & Conditions";
+        
